d97f6b55e6606eb118cfc82850a19bb0c23971b4
chooblarin <choo.bla.rin@gmail.com>
2017-10-28 16:39:31 +0900

    Update website

--- src/components/App.js
+++ src/components/App.js
@@ -9,47 +9,69 @@ const App = () => (
     <Intro />
     <main>
 
-      <h2><i class="twa twa-2328"></i> コンテンツ</h2>
+      <h1><i className="twa twa-1f3a9"></i> コンテンツ</h1>
 
-      <h3>1日目</h3>
+      <div className="container-day day01">
 
-      <ul>
-        <li>Processingで「Hello world」</li>
-        <li>色を変えてみる</li>
-        <li>図形を書いてみる</li>
-        <li>線を書いてみる</li>
-        <li>おもしろい線を書いてみる</li>
-        <li>図形を動かそう</li>
-        <li>インタラクティブな描画</li>
-      </ul>
+        <h2><i className="twa twa-1f423"></i> 1日目 (2017/11/03)</h2>
 
-      <h3>2日目</h3>
+        <h3>テーマ</h3>
 
-      <ul>
-        <li>乱数を使ってみよう</li>
-        <li>パーティクル</li>
-        <li>フラクタル</li>
-        <li>自由に書いてみよう</li>
-      </ul>
+        <p className="description">Processingで実験的にお絵かきしていくための基本的な知識と直感を身につけましょう。</p>
 
-      <h3>3日目</h3>
-      <ul>
-        <li>自由に書いてみよう2</li>
-        <li>発表会</li>
-      </ul>
+        <h3>目次</h3>
 
-      <h2><i class="twa twa-1f4da"></i> 学習</h2>
+        <ul className="toc">
+          <li>Processingで「Hello world」</li>
+          <li>色を変えてみる</li>
+          <li>図形を書いてみる</li>
+          <li>線を書いてみる</li>
+          <li>ランダムを使う</li>
+          <li>図形を動す</li>
+          <li>インタラクティブなスケッチ</li>
+          <li>自由に描いてみましょう</li>
+        </ul>
 
-      <ul>
-        <li><a href="http://natureofcode.com/book/">The Nature of Code</a></li>
-        <li><a href="https://www.openprocessing.org/">OpenProcessing - Algorithmic Designs Created with Processing</a></li>
-      </ul>
+      </div>
 
-      <h2><i class="twa twa-2728"></i>リンク</h2>
+      <div className="container-day day02">
 
-      <ul>
-        <li><a href="https://www.city.niigata.lg.jp/business/kigyo/sokushinho/it_contents/kigyo20170707.html">新潟市におけるプログラミング教育の取り組み</a></li>
-        <li><a href="http://www.codeforniigata.org/">Code for Niigata | コードを紡いで、地域を繋ぐ</a></li>
+        <h2><i className="twa twa-1f425"></i> 2日目 (2017/11/04)</h2>
+
+        <h3>テーマ</h3>
+        <p className="description">おもしろい模様や綺麗なグラフィックを描くための材料を探してみましょう。</p>
+
+        <h3>目次</h3>
+
+        <ul className="toc">
+          <li>ノイズ</li>
+          <li>パーティクル</li>
+          <li>フラクタル</li>
+          <li>自由に描いてみましょう2</li>
+        </ul>
+
+      </div>
+
+      <div className="container-day day02">
+        <h2><i className="twa twa-1f426"></i> 3日目 (2017/11/05)</h2>
+
+        <h3>テーマ</h3>
+        <p className="description">自分の作品を発表しましょう。</p>
+
+        <h3>目次</h3>
+        <ul className="toc">
+          <li>自由に描いてみましょう3</li>
+          <li>発表会</li>
+        </ul>
+      </div>
+
+      <h1><i className="twa twa-2728"></i> 関連・参考リンク</h1>
+
+      <ul className="reference">
+        <li><a href="https://www.city.niigata.lg.jp/business/kigyo/sokushinho/it_contents/kigyo20170707.html" target="_blank">新潟市におけるプログラミング教育の取り組み</a></li>
+        <li><a href="http://www.codeforniigata.org/" target="_blank">Code for Niigata | コードを紡いで、地域を繋ぐ</a></li>
+        <li><a href="http://natureofcode.com/book/" target="_blank">The Nature of Code</a></li>
+        <li><a href="https://www.openprocessing.org/" target="_blank">OpenProcessing - Algorithmic Designs Created with Processing</a></li>
       </ul>
     </main>
     <Footer />
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,8 +6,8 @@ const Intro = () => (
   <div className="intro">
     <div className="gatamaru"></div>
     <section>
-      <h1 className="title">がたまるプログラミングキャンプ</h1>
-      <p className="sub-title">Processingでビジュアルコーディング</p>
+      <h1 className="title">Processingでビジュアルコーディング</h1>
+      <p className="sub-title">がたまるプログラミングキャンプ2017</p>
       <p className="description">
         このコースではコーディングでお絵かきをします．お絵かきには<a href="https://processing.org/"> "Processing" </a>を使用します．
         線や図形を描画して，おもしろい模様や綺麗なグラフィックを描いてみましょう．
