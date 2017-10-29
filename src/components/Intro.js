8d358e731515076ee1e8cc1d5b5e069088826d8a
chooblarin <choo.bla.rin@gmail.com>
2017-10-29 23:17:43 +0900

    Modify intro description a bit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,7 @@ const Intro = () => (
       <h1 className="title">Processingでビジュアルコーディング</h1>
       <p className="sub-title">がたまるプログラミングキャンプ2017</p>
       <p className="description">
-        このコースではコーディングでお絵かきをします．お絵かきには<a href="https://processing.org/"> "Processing" </a>を使用します．
+        このコースではコーディングでお絵描きをします．お絵描きには<a href="https://processing.org/"> "Processing" </a>を使用します．
         線や図形を描画して，おもしろい模様や綺麗なグラフィックを描いてみましょう．
       </p>
     </section>
