6cd4288780d48e455b2f3fab19be04aba8c75888
chooblarin <choo.bla.rin@gmail.com>
2017-11-05 23:30:38 +0900

    Fix copyright footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 
 const Footer = () => (
   <footer>
-    <p className="copyright">© 2017 chooblarin <i className="twa twa-2665"></i> Niigata </p>
+    <p className="copyright">© 2017 gatamaru <i className="twa twa-2665"></i> Niigata </p>
   </footer>
 )
 
